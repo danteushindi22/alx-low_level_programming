57c65de651230b0bb4af011576e7eb45b2af7eb7
danteushindi22 <danielushindi16@gmail.com>
2022-06-03 09:43:00 -0700

    8 print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	int num;
+
+	for (num = 0; num < 16; num++)
+	{
+		if (num < 10)
+		{
+			putchar(num + '0');
+		} else
+		{
+			putchar(a);
+			a++;
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}

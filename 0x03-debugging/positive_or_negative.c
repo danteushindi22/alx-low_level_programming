5ba948bd80f0f49ece03e9432fca6c2f228b735a
danteushindi22 <danielushindi16@gmail.com>
2022-06-09 13:54:11 -0700

    positive or negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * positive_or_negative - checks whether num is pos or neg
+ * @n: number to be checked
  *
  * Return: Always 0 (Success)
  */
 
 void positive_or_negative(int n)
 {
-	int n;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
@@ -25,5 +24,4 @@ void positive_or_negative(int n)
 	{
 		printf("%d is positive\n", n);
 	}
-	return (0);
 }
